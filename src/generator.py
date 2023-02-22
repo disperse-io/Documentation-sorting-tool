fea9c41cfc8e6cbb7ea647e527d257ac62a4a495
saninnn <sanin@disperse.io>
2023-02-22 15:59:38 +0100

    Code Optimization

--- src/generator.py
+++ src/generator.py
@@ -31,7 +31,7 @@ class Generator:
         # Creating Folders
         for directory_name in self.folder_names:
             if directory_name == "02_Typical Floor":
-                for floor_no in range(00, 51):
+                for floor_no in range(-4, 51):
                     os.makedirs(
                         os.path.join(
                             project_folders_path,
@@ -45,7 +45,11 @@ class Generator:
                 os.makedirs(
                     os.path.join(project_folders_path, directory_name),
                     exist_ok=True,
-                )
+                )          
+
+
+        
+
 
         for root, dirs, files in os.walk(self.source_dir_path):
             for file in files:
@@ -76,6 +80,8 @@ class Generator:
                 + " non-pdf files has been moved to 11_Others folder"
             )
         display("Folder created to following path: " + str(project_folders_path))
+
+       
 
     def fill_excel_sheet(self):
         all_data_files = os.listdir(
@@ -206,21 +212,15 @@ class Generator:
         path_to_typical_floor=os.path.join(self.destination_dir_path,self.project_name,"02_Typical Floor")       
         files_and_folders = os.listdir(os.path.join(self.destination_dir_path,self.project_name,"02_Typical Floor"))
 
-                
-        # display(files_and_folders)
+       
 
         for item in files_and_folders:
             folder_path = os.path.join(path_to_typical_floor,item)
             folder_path = folder_path.replace("\\", "/")
-            pdf_file_list=os.listdir(folder_path)                   
-
-            # display("First Step on road !!")
-            display(folder_path)
-            # display(pdf_file_list)
-
+            pdf_file_list=os.listdir(folder_path)             
             JPEG_Folder_path=os.path.join(folder_path,"JPEGs")
             JPEG_Folder_path=JPEG_Folder_path.replace("\\", "/")
-            display(JPEG_Folder_path)
+        
             if os.path.isdir(JPEG_Folder_path):
                 display(f'Folder at {item} already exist')
                 pass
@@ -243,78 +243,7 @@ class Generator:
                             img.save(setupPath +"_" +str(counter) + ".jpg", "JPEG")
                             counter += 1
                     display(f'Converted to {file}.jpg') 
-                display('All pdf are converted successfully')                  
+            display('All pdf are converted successfully')                  
                     
            
-                
-            # for pdf_file in pdf_file_list:                    
-            #     pdf_path=os.path.join(item,pdf_file)
-            #     try:
-            #         with open(pdf_path,'rb') as f:
-            #             pages = convert_from_path(pdf_path)
-            #         for i, page in enumerate(pages):
-            #             jpeg_path=os.path.join(item,f"{pdf_file}_{i}.jpg")
-            #             page.save(jpeg_path,'JPEG')
-
-            #     except IOError as e:
-            #         print(f"I/O Error: {e}")
-                    
-
-                 
-                    
-
-
-
-                   
-
-                    # for i, page in enumerate(pages):
-                    #     jpeg_path = os.path.join(JPEG_Folder_path,f"{pdf_file}_{i}.jpg")
-                    #     page.save(jpeg_path,"JPEG")
-
-
-
-        #         print(folder_path)
-        #         print(pdf_file_list)
-
-        
-
-
-
-        
-        # for item in folder_path:
-        #     print(item)
-            # file_list=os.listdir(folder)
-            # print(file_list)
-
-
-
-
-
-            # pdf_files = [f for f in os.listdir(folder_path) if f.endswith('.pdf')]
-            # display('First Loop')
-            # for pdf_file in pdf_files:
-            #     pdf_path = os.path.join(folder, pdf_file)
-            #     pages = convert_from_path(pdf_path)
-            #     display("Second Loop")
-            #     for i, page in enumerate(pages):
-            #         jpeg_path = os.path.join(folder, f"{pdf_file}_{i}.jpg")
-            #         page.save(jpeg_path, 'JPEG')
-            #         display("3rd Loop")
-        # with open(pdf_path, 'rb') as f:
-        #     pdf = PyPDF2.PdfFileReader(f)
-        #     for page_num in range(pdf.getNumPages()):
-        #         page = pdf.getPage(page_num)
-        #         _, temp_filename = tempfile.mkstemp()
-        #         with open(temp_filename, 'wb') as f:
-        #             f.write(page.contentStream.getData())
-        #         img = Image.open(temp_filename)
-        #         output_filename = os.path.splitext(os.path.basename(pdf_path))[0] + f'_page_{page_num+1}.jpg'
-        #         img.save(os.path.join(output_folder, output_filename), 'JPEG')
-        #         os.remove(temp_filename)
-
-        # project_folders_path = os.path.join(
-        #     self.destination_dir_path, self.project_name
-        # )
-        # folder_path = project_folders_path + "02_Typical Floor"
-        # Display("This is folder path : ", folder_path)
-        # output_folder
+          
